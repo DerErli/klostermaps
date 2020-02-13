0501a063c944ac99bcef34fbaccf036522b1de48
DerErli <valentin.schmitz2@gmail.com>
2020-02-13 16:38:28 +0100

    Refactored map again (upload route)

--- routes/map.js
+++ routes/map.js
@@ -33,12 +33,74 @@ router.post(
       .isString()
       .isLength({ max: 280 })
       .escape(),
+    body('mapFileName')
+      .isString()
+      .isLength({ max: 280 })
+      .escape(),
     body('markers')
       .optional()
       .isArray(),
     body('polylines')
       .optional()
       .isArray(),
+    header('authorization')
+      .isString()
+      .bail()
+      .custom(value => {
+        const tkn = value.split(' ')[1];
+        try {
+          return jwt.verify(tkn, webTkn);
+        } catch (err) {
+          return Promise.reject('Invalid Token');
+        }
+      })
+  ],
+  async (req, res) => {
+    //validation
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    //params
+    var name = req.body.name;
+    var description = req.body.description ? req.body.description : 'New map!';
+    var markers = req.body.markers ? req.body.markers : [];
+    var polylines = req.body.polylines ? req.body.polylines : [];
+
+    //image
+    let mapFileName = req.body.mapFileName;
+    let data = fs.readFileSync(path.resolve('./userUploads', mapFileName));
+    data = Buffer.alloc(data.byteLength, data, 'binary').toString('base64');
+    var mapRawImage = { data: String(data), contentType: path.extname(mapFileName) };
+
+    var newMap = new Map({
+      name,
+      description,
+      mapFileName,
+      mapRawImage,
+      markers,
+      polylines
+    });
+
+    //try to save map
+    try {
+      await newMap.save();
+      map = newMap.toJSON();
+      delete map.image;
+      res.json({ msg: 'Map saved', map });
+    } catch (err) {
+      res.status(500).json({ msg: 'Internal server error', err: err });
+    }
+  }
+);
+
+// @route POST api/map/upload
+// @desc POST Upload image
+// @access Protected (token required)
+router.post(
+  '/upload',
+  [
     header('authorization')
       .isString()
       .bail()
@@ -67,45 +129,20 @@ router.post(
       }
     }
 
-    //params
-    var name = req.body.name;
-    var description = req.body.description ? reqreq.body.description : 'New map!';
-    var markers = req.body.markers ? req.body.markers : [];
-    var polylines = req.body.polylines ? req.body.polylines : [];
-
-    //rename image
+    //rename
     let temp = req.files.mapImage.tempFilePath;
     let type = req.files.mapImage.mimetype;
     let tempNew = path.resolve('./tmp', uuid() + '.' + type.replace('image/', ''));
     fs.renameSync(temp, tempNew);
 
-    //read image file
-    let data = fs.readFileSync(tempNew);
-    data = Buffer.alloc(data.byteLength, data, 'binary').toString('base64');
-
-    //move and prepare image file to be saved
+    //move
     mv(tempNew, path.resolve('./userUploads', path.basename(tempNew)), err => {
       if (err) console.error(err);
     });
 
-    var mapRawImage = { data: String(data), contentType: type };
-    var mapFileName = path.basename(tempNew);
-
-    var newMap = new Map({
-      name,
-      description,
-      mapFileName,
-      mapRawImage,
-      markers,
-      polylines
-    });
-
-    //try to save map
+    //save map
     try {
-      await newMap.save();
-      map = newMap.toJSON();
-      delete map.image;
-      res.json({ msg: 'Map saved', map });
+      res.json({ msg: 'Image saved', fileName: path.basename(tempNew) });
     } catch (err) {
       res.status(500).json({ msg: 'Internal server error', err: err });
     }
